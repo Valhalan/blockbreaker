7e73c1eae88c8d5f49e0405f675e1ef24d945f28
Valhalan <40497187+Valhalan@users.noreply.github.com>
2020-04-21 15:27:08 +0100

    Basic Level created with collision and deletion of blocks

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 0.75f;
+    [SerializeField] float maxX = 15.25f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float mouseXPosition = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Debug.Log(mouseXPosition);
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mouseXPosition, minX, maxX);
+        transform.position = paddlePosition;
+	}
+}
